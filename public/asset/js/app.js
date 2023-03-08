574bf896f14f3653756919d9f67c1bd14f820a5c
Your Name201~ <abolore200@gmail.com>
2023-03-08 23:32:08 +0100

    updated

--- public/asset/js/app.js
+++ public/asset/js/app.js
@@ -4,3 +4,10 @@ const month = fullDate.toLocaleString('default', { month: 'long' })
 const getYear = fullDate.getFullYear()
 const inputDate = document.querySelector('.ms-cd-date p')
 inputDate.innerHTML = `${getDate} ${month}, ${getYear}`
+
+document.addEventListener('DOMContentLoaded', function(){
+    const homeLoader = document.querySelector('.ms-cd-fixed-navbar-col-row-1 p')
+    homeLoader.style.backgroundColor = 'rgba(0,0,255,0.7)'
+    const homeLoaderI = document.querySelector('.ms-cd-fixed-navbar-col-row-1 p a')
+    homeLoaderI.style.color = '#fff'
+})
