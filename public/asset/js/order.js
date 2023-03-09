739ee32eb5953ec704414f403e209d0081bbbf99
Your Name201~ <abolore200@gmail.com>
2023-03-09 21:02:43 +0100

    updated

--- public/asset/js/order.js
+++ public/asset/js/order.js
@@ -1,22 +1,45 @@
-const form = document.querySelector('form')
-form.addEventListener('submit', function(e){
-    e.preventDefault()
-
+const form = document.querySelector('.submitForm')
+form.addEventListener('submit', function(){
     const itemType = document.querySelector('.yd-col-3-item select').value
     const chooseDate = document.querySelector('.col-4-input-date input[type="date"]').value
 
     if(itemType === '' || chooseDate === ''){
         submitform.errorMessage()
+    } else {
+        submitform.confirmOrder(itemType,chooseDate)
     }
 })
 
 class serviceForm{
     errorMessage(){
-        const errorNote = document.createElement('div')
-        const marginAuto = document.querySelector('.df-gh-yd-col-1')
+        const marginAuto = document.querySelector('form .submit-btn')
+        const errorNote = document.createElement('div') 
+        errorNote.classList.add('p-margin')       
 
-        errorNote.innerHTML = `<p> All Fields Are Mandatory </p>`
+        errorNote.innerHTML = `<p> All Fields Are Mandatory! </p>`
         form.insertBefore(errorNote,marginAuto)
+        setTimeout(function(){
+            errorNote.remove()
+        }, 3000)
+    }
+    confirmOrder(itemType,chooseDate){
+        const arrayObject = {itemType,chooseDate}
+        getToLs(arrayObject)
     }
 }
 const submitform = new serviceForm()
+//
+function getToLs(arrayObject){
+    let dateItem = addToLS()
+    dateItem.push(arrayObject)
+    localStorage.setItem('itemDate', JSON.stringify(dateItem))
+}
+function addToLS(){
+    let dateItem;
+    let dateItemLs = localStorage.getItem('itemDate')
+    if(dateItemLs === null){
+        dateItem = []
+    } else {
+        dateItemLs = JSON.parse(dateItemLs)
+    } return dateItem
+}
